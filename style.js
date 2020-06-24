1cfa60bfa4a0487a2f6eaea3784591c4910e6724
LLenroc <nc_2@hotmail.com>
2020-06-24 19:08:44 -0400

    added function to initialize schedule for array.  not defined. Figuring out

--- style.js
+++ style.js
@@ -2,10 +2,20 @@
 
 var $currentDay = $("#currentDay");
 var $schedule = $(".schedule");
-var $timeBlock = $(".time-block");
+var $timeBlocks = $(".time-block");
 
 var toDos = [];
 
 // format for current date and hour using moment.js
+
 var currentDate = moment().format("dddd, MMM Do");
 var currentHour = moment().format("H");
+
+// Added function - to set up array
+
+function initializeSchedule() {
+
+// added a "for each" for the time-blocks
+    $timeBlocks.each
+
+}
