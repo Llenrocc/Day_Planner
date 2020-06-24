672419407f8264140073d53deaedfe9875ae574f
LLenroc <nc_2@hotmail.com>
2020-06-24 18:46:10 -0400

    current date and hour with moment.js format

--- style.js
+++ style.js
@@ -3,3 +3,9 @@
 var $currentDay = $("#currentDay");
 var $schedule = $(".schedule");
 var $timeBlock = $(".time-block");
+
+var toDos = [];
+
+// format for current date and hour using moment.js
+var currentDate = moment().format("dddd, MMM Do");
+var currentHour = moment().format("H");
